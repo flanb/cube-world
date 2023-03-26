69f8c51318e13238a64759c5da7af0e66592ce38
FlanB <clement.renou@mmibordeaux.com>
2023-03-26 01:49:00 +0100

    ✨(PhysicsWorld): add cannon-es debugger

--- src/WebGL/PhysicsWorld.js
+++ src/WebGL/PhysicsWorld.js
@@ -1,7 +1,10 @@
 import Experience from "./Experience.js";
 import * as CANNON from "cannon-es";
-export default class PhysicsWorld {
+import CannonDebugger from "cannon-es-debugger";
+import EventEmitter from "utils/EventEmitter.js";
+export default class PhysicsWorld extends EventEmitter {
   constructor() {
+    super();
     this.experience = new Experience();
     this.debug = this.experience.debug;
 
@@ -53,6 +56,27 @@ export default class PhysicsWorld {
       .on("change", () => {
         this.instance.gravity.z = this.gravity.z;
       });
+
+    this.debugFolder
+      .addButton({
+        title: "cannonDebuggerVisible",
+      })
+      .on("click", () => {
+        if (!this.cannonDebugger) {
+          this.cannonDebugger = new CannonDebugger(
+            this.experience.scene,
+            this.instance,
+            {
+              onInit: (body, mesh) => {
+                this.on("cannonDebuggerVisible", () => {
+                  mesh.visible = !mesh.visible;
+                });
+              },
+            }
+          );
+        }
+        this.trigger("cannonDebuggerVisible");
+      });
   }
 
   setInstance() {
@@ -67,5 +91,6 @@ export default class PhysicsWorld {
 
   update() {
     this.instance.step(1 / 60, this.experience.time.delta, 3);
+    if (this.cannonDebugger) this.cannonDebugger.update();
   }
 }
